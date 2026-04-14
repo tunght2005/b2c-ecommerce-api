8b704b53b7893fad26abeac81ddbb0085bf01c6f
Tung DEV <tunght2005@gmail.com>
2026-04-15 00:31:02 +0700

    refactor: add cors on index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,31 @@ const connectDB = require('./database/db')
 
 const app = express()
 
+const allowedOrigins = [
+  /^http:\/\/localhost:\d+$/,
+  /^https:\/\/.+\.onrender\.com$/,
+  /^https:\/\/b2c-fe-admin\.onrender\.com$/
+]
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin
+
+  if (!origin || allowedOrigins.some((pattern) => pattern.test(origin))) {
+    res.header('Access-Control-Allow-Origin', origin || '*')
+    res.header('Vary', 'Origin')
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(204)
+    }
+
+    return next()
+  }
+
+  return res.status(403).json({ message: 'CORS blocked' })
+})
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
